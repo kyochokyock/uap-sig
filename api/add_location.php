408769efd35e74c293a82aa7916bad306964e393
goji <96336079+kyochokyock@users.noreply.github.com>
2025-12-12 04:40:06 +0700

    Add files via upload

---
+++ api/add_location.php
@@ -0,0 +1,38 @@
+<?php
+header('Content-Type: application/json');
+
+$file = '../data/pendidikan.geojson';
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (!$input || !isset($input['namobj']) || !isset($input['latitude']) || !isset($input['longitude'])) {
+    echo json_encode(['success' => false, 'message' => 'Data tidak lengkap']);
+    exit;
+}
+
+$data = json_decode(file_get_contents($file), true);
+
+$newFeature = [
+    "type" => "Feature",
+    "properties" => [
+        "NAMOBJ" => $input['namobj'],
+        "LUAS" => 0.0,
+        "REMARK" => $input['remark']
+    ],
+    "geometry" => [
+        "type" => "Point",
+        "coordinates" => [
+            floatval($input['longitude']),
+            floatval($input['latitude']),
+            0.0
+        ]
+    ]
+];
+
+$data['features'][] = $newFeature;
+
+if (file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT))) {
+    echo json_encode(['success' => true, 'message' => 'Lokasi berhasil ditambahkan']);
+} else {
+    echo json_encode(['success' => false, 'message' => 'Gagal menyimpan data']);
+}
+?>
